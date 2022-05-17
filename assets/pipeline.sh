3efd1349ebc53c6fd4490e17bd76609257291b49
ruvin <rk241@students.waikato.ac.nz>
2022-05-17 18:08:04 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,7 +1,7 @@
 # ruvin Kokuhennadige
 #1541447
 
-echo"(0) npm installing"
+echo "(0) npm installing"
 if npm install; then
     echo"npm successfully installed"
 else
@@ -9,7 +9,7 @@ else
     exit
 fi
 
-echo"(1) building"
+echo "(1) building"
 if npm run build; then
     echo"build successfull "
 else
@@ -17,5 +17,20 @@ else
     exit
 fi
 
-echo "(2) application start "
+echo "(2) commit and pushing to repo"
+message="$1"
+if [[ -z "${message// }" ]]
+    then
+        message=$"COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+    
+
+fi
+
+cd ..
+git add .
+git commit -m "$message"
+git push
+
+echo "(3) application start "
+cd assets
 npm run start
